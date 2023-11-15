3b67250c8e3681e4b4a88948e627e0b48eff1d0c
Carlos Gómez <drcgomez@hotmail.com>
2023-11-14 21:13:30 -0500

    Input para recibir tipo de color

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html'
 })
-export class ButtonComponent {}
+export class ButtonComponent implements OnChanges {
+
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  @Input() color: string = 'primary';
+  classButton: string = '';
+
+  ngOnChanges(changes: SimpleChanges) {
+    const color: SimpleChange = changes['color']?.currentValue;
+
+    if (color) {
+      this.classButton = `bg-${this.color}-700 hover:bg-${this.color}-800`;
+      console.log(this.classButton);
+    }
+  }
+}
