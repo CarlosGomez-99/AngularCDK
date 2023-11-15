fe8c4b618fe77270f5bbb75bbca559dd79a689e9
Carlos Gómez <drcgomez@hotmail.com>
2023-11-14 20:41:59 -0500

    creación componente boton para reutilización

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html'
+})
+export class ButtonComponent {}
