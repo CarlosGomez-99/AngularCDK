8670b7aca3277588cbf2a8f0949239eba76c8acd
Carlos Gómez <drcgomez@hotmail.com>
2023-11-14 20:41:28 -0500

    Extension de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        success: colors.green,
+        primary: colors.blue,
+        gray: colors.trueGray,
+        blue: colors.lightBlue,
+        red: colors.rose,
+        pink: colors.fuchsia,
+      },
+    },
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-  ],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
