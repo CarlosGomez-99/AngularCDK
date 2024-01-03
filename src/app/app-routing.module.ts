b323221dc5cf1ccee049bf14614dd6b45fafe4e6
carlos.gomez <drcgomez@hotmail.com>
2024-01-02 21:49:45 -0500

    Creación de overlays

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
+import { BoardsComponent } from './components/boards/boards.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'boards', component: BoardsComponent },
   { path: '', redirectTo: '/login', pathMatch: "full" },
   { path: '**', redirectTo: '/login', pathMatch: "full" }
 ];
